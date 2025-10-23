70275ddd95b921bbe1c077ce3b715cb4e71f0d6e
Sultan <sultanmulani65@gmail.com>
2025-10-23 20:25:50 +0530

    Updating Styles

--- src/App.jsx
+++ src/App.jsx
@@ -14,61 +14,55 @@ export default function App(){
           </button>
         </div>
       </nav> */}
-      <nav class="navbar bg-body-tertiary fixed-top">
+      
+      {/* <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-nav">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">King Sukh Guest House</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="#home">Kingsukh Guest House</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Welcome</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              More
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Reviews</a></li>
-              <li><a class="dropdown-item" href="#">Blogs</a></li>
-              <li>
-                <hr class="dropdown-divider" />
-              </li>
-              <li><a class="dropdown-item" href="#">Contact Us</a></li>
-            </ul>
-          </li>
-        </ul>
-        {/* <form class="d-flex mt-3" role="search"> */}
-          {/* <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/> */}
-          <button className="btn btn-light"
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blogs</a>
+        </li>
+      </ul>
+        <button className="btn btn-light"
             onClick={() => document.getElementById('book').scrollIntoView({ behavior: 'smooth' })}>
-            Book Now 
+            Book Now
           </button>
-        {/* </form> */}
-      </div>
     </div>
   </div>
-</nav>
+</nav> */}
+      
+      <nav class="nav-new" >
+        <h5 class="jp"><a href="#">Kingsukh Guest House</a></h5>
+        <ul class="main-nav">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About us</a></li>
+        </ul>
+        <button className="btn btn-light"
+            onClick={() => document.getElementById('book').scrollIntoView({ behavior: 'smooth' })}>
+            Book Now
+          </button>
+    </nav> 
 
-      <header className="hero" style={{backgroundImage:'url(/images/ayodhya.webp)'}}>
+      <header className="hero" style={{backgroundImage:'url(/images/ayodhya.webp)'}} id="home">
         <div className="overlay"></div>
-        <div className="hero-content container">
+        <div className="hero-content container" >
           <h2>Welcome to King Sukh Guest House</h2>
           <p>Comfort · Cleanliness · Hospitality</p>
           <button className="btn btn-light"
             onClick={() => document.getElementById('book').scrollIntoView({ behavior: 'smooth' })}>
             Check Availability
           </button>
-          {/* <a className="btn btn-light" href="#book">Check Availability</a> */}
         </div>
       </header>
 
@@ -191,7 +185,7 @@ function BookingForm(){
       <label>Message
         <textarea name="message" value={state.message} onChange={handle}></textarea>
       </label>
-      <button className="btn" type="submit">Send Booking</button>
+      <button className="btn btn-light" type="submit">Send Booking</button>
       {sent && <div className="success">Thanks — booking request sent.</div>}
     </form>
   )
